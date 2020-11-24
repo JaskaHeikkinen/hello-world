539dbd00920744cd61a94d1861a3e942dbe6b810
Sagar <acharyasaagar@gmail.com>
2020-11-24 10:22:11 +0200

    add cors, projects

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
+const projects = require('./projects')
 
 const app = express()
+app.use(cors())
 
 const PORT = process.env.PORT || 8080
 
-app.get('/', (req, res) => res.send('Hello world'))
+app.get('/', (req, res) => res.json(projects))
 
 app.listen(PORT, () => console.log('Server is listening at port', PORT))
